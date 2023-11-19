e7a7cb720a8cd11fa41b969c755aad989fa5dbce
Gagana1312 <ananda.g@northeastern.edu>
2023-11-19 01:15:11 -0500

    index.html

--- backend.py
+++ backend.py
@@ -5,7 +5,7 @@ import random
 import string
 
 
-from flask import Flask, render_templet, redirect, url_for, request
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 shorten_url = {}
@@ -30,7 +30,7 @@ def index():
             short_url = generate_short_URL()
         shorten_url[short_url] = long_url
         return f"Shortened URL :{request.url_root}{short_url}"
-    return render_templet("index.html")
+    return render_template("index.html")
 
 @app.route("/<short_url>")
 def redirect_url(short_url):
